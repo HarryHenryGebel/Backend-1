2cde316e54d0e76894bfcd117b91239d260cc435
Jeremiah Trnka <jeremiah@jeremiahtrnka.com>
2020-09-19 16:50:42 -0700

    Update CategoryRepository

--- foundation/src/main/java/com/lambdaschool/marketplace/repository/CategoryRepository.java
+++ foundation/src/main/java/com/lambdaschool/marketplace/repository/CategoryRepository.java
@@ -1,4 +1,9 @@
 package com.lambdaschool.marketplace.repository;
 
-public interface CategoryRepository {
+import com.lambdaschool.marketplace.models.Category;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends CrudRepository<Category, Long> {
 }

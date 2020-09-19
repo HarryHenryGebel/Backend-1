df2c9cf78008e74fa7e5260e7c582a4c98bd2322
Jeremiah Trnka <jeremiah@jeremiahtrnka.com>
2020-09-19 14:18:49 -0700

    Added ItemRepository.java

---
+++ foundation/src/main/java/com/lambdaschool/marketplace/repository/ItemRepository.java
@@ -0,0 +1,29 @@
+package com.lambdaschool.marketplace.repository;
+
+import com.lambdaschool.marketplace.models.Item;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * The CRUD repository connecting Item to the rest of the application
+ */
+@Repository
+public interface ItemRepository extends CrudRepository<Item, Long> {
+
+    /**
+     * Find a list of items associated with a specific user
+     *
+     * @param userId the userId of the user assigned to each item
+     * @return a list of items assigned to the user
+     */
+    List<Item> findByUserId(long userId);
+
+    /**
+     * Find a list of all items
+     *
+     * @return a list of all items
+     */
+    List<Item> findAllItems();
+}

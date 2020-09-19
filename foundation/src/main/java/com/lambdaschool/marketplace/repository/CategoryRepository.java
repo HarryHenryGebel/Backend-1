b2879b104ed189fc6ced96abf0091e3bbd5644d7
Jeremiah Trnka <jeremiah@jeremiahtrnka.com>
2020-09-19 16:53:25 -0700

    Completed CategoryRepository

--- foundation/src/main/java/com/lambdaschool/marketplace/repository/CategoryRepository.java
+++ foundation/src/main/java/com/lambdaschool/marketplace/repository/CategoryRepository.java
@@ -4,6 +4,14 @@ import com.lambdaschool.marketplace.models.Category;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    /**
+     * Find a list of all categories
+     * @return a list of all categories
+     */
+    List<Category> findAllCategories();
 }

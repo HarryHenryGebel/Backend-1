9f8388b08f6d782c8ea47f202b4c0c88ed7b6c9c
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-19 22:15:41 -0400

    Create ProductRepository repository

---
+++ foundation/src/main/java/com/lambdaschool/marketplace/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.lambdaschool.marketplace.repository;
+
+import com.lambdaschool.marketplace.models.Product;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Long> {}

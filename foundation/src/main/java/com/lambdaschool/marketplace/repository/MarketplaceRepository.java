9db80029e0a13985d3f8342a6acde73864be689b
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-19 21:56:13 -0400

    Make bean

--- foundation/src/main/java/com/lambdaschool/marketplace/repository/MarketplaceRepository.java
+++ foundation/src/main/java/com/lambdaschool/marketplace/repository/MarketplaceRepository.java
@@ -2,6 +2,8 @@ package com.lambdaschool.marketplace.repository;
 
 import com.lambdaschool.marketplace.models.Marketplace;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MarketplaceRepository
   extends CrudRepository<Marketplace, Long> {}

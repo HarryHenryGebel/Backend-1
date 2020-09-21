694467cd6e6197bdf3d6e821a57acca818badef6
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-21 17:36:25 -0400

    prettier

--- foundation/src/main/java/com/lambdaschool/marketplace/models/Product.java
+++ foundation/src/main/java/com/lambdaschool/marketplace/models/Product.java
@@ -1,10 +1,9 @@
 package com.lambdaschool.marketplace.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
+import javax.persistence.*;
 
 /**
  * The entity allowing interaction with the categories table
